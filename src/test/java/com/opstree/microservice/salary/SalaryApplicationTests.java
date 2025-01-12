1fd96ddcf4cac81c41da3cd9aea3492a1a50fc7d
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-01-13 00:21:06 +0530

    Update SalaryApplicationTests.java

--- src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
+++ src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -2,8 +2,9 @@ package com.opstree.microservice.salary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = SalaryApplication.class)
+@SpringBootTest(classes = SalaryApplicationTests.class)
 public class SalaryApplicationTests {
 
     @Test
